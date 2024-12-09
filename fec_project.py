599965ba6c315e849904a566f8b31ebee9e234ff
Christian Joeffrey Castro <castrochristianjoeffrey10@gmail.com>
2024-12-09 10:45:58 +0800

    - deleted extra py file - Added Partial pivoting

--- fec_project.py
+++
@@ -1,9 +0,0 @@
-'''
-FEC circuit analysis project
-
-Gaussian Elimination
-GUI part
-'''
-
-if __name__ == "__main__":
-    pass
--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,5 +1,10 @@
+from fractions import Fraction
 import numpy as np
+import tkinter as tk
+from tkinter import ttk
 
+def x():
+    pass
 
 def is_square_matrix(matrix:np.ndarray) -> bool:
     '''Checks if matrix is a square matrix'''
@@ -32,12 +37,28 @@ def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> np.ndarray:
     augmented_matrix: np.ndarray = np.concatenate((matrix_a, matrix_b), axis=1).astype(float)
 
     #? Debugging: show augemented matrix
+    print("Augemented Matrix (Initial): ")
     print(augmented_matrix)
     print()
 
+
+    #* partial pivot
     for i in range(matrix_b.size):
-        #! 0 on pivot points
-        #todo: partial pivot
+
+        max_row = i + np.argmax(abs(augmented_matrix[i:, i]))
+        if max_row != i:
+
+            augmented_matrix[[i, max_row]] = augmented_matrix[[max_row, i]]
+            print(f"Swapped rows {i} and {max_row}:")
+            print(augmented_matrix)
+            print()
+
+    print("Solution:")
+
+
+    for i in range(matrix_b.size):
+
+        #! 0 on pivot points (unsolvable)
         if augmented_matrix[i][i] == 0.0:
             raise ZeroDivisionError
 
@@ -50,6 +71,7 @@ def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> np.ndarray:
 
             #? Debugging: show augmented matrix
             # print(f"{i}, {j}, {augmented_matrix[j][i]}, {augmented_matrix[i][i]}, {scaling_factor}")
+            print(f"Scaling factor: {scaling_factor}")
             print(augmented_matrix)
             print()
 
@@ -74,26 +96,42 @@ def gaussian_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> None:
 
     aug_matrix = forward_elimination(matrix_a,matrix_b)
     answers = backwards_substitution(aug_matrix,aug_matrix.shape[1] - 1)
+    ans_frac = [Fraction(x).limit_denominator() for x in answers]
 
     print(answers)
+    print(ans_frac)
 
     return None
 
 
-#? TEST CASES
-# a = np.array([[1,1,3,1],
-#               [0,1,3,4],
-#               [-1,3,0,2],
-#               [8,1,4,2]])
-# b = np.array([[1],
-#               [3],
-#               [5],
-#               [4]])
-# c = np.array([[10,11,12],
-#               [13,14,15],
-#               [16,17,45]])
-# d = np.array([[3],
-#              [5],
-#              [1]])
+def main() -> None:
+    # a = np.array([[1,1,3,1],
+    #               [0,1,3,4],
+    #               [-1,3,0,2],
+    #               [8,1,4,2]])
+    # b = np.array([[1],
+    #               [3],
+    #               [5],
+    #               [4]])
+    # c = np.array([[10,11,12],
+    #               [13,14,15],
+    #               [16,17,45]])
+    # d = np.array([[3],
+    #              [5],
+    #              [1]])
 
-# gaussian_elimination(c,d)
+    # a = np.array([[Fraction(3,20),Fraction(-1,8)],
+    #               [Fraction(-1,8),Fraction(7,48)]])
+    # b = np.array([[Fraction(6,1)],
+    #               [Fraction(-1,1)]])
+    a = np.array([[0,-5,-10],
+                  [45,0,-4],
+                  [-10,-4,0]])
+    b = np.array([[130],
+                  [106],
+                  [0]])
+    gaussian_elimination(a,b)
+    return None
+
+if __name__ == "__main__":
+    main()
