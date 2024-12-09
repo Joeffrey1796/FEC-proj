94978090aa837c16a903b2f1e07dd40a48b429e5
Christian Joeffrey Castro <castrochristianjoeffrey10@gmail.com>
2024-12-09 19:12:55 +0800

    Sol'n as strings

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -1,7 +1,5 @@
 from fractions import Fraction
 import numpy as np
-import tkinter as tk
-from tkinter import ttk
 
 def x():
     pass
@@ -28,18 +26,19 @@ def backwards_substitution(matrix:np.ndarray,size) -> np.ndarray:
 
         x[i] = sum_ / matrix[i][i]
 
-    return x
+    return (x, str(x))
 
 
-def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> np.ndarray:
+def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> tuple[np.ndarray, list[str]] :
     '''Creates and modifies the augmented matrix with the upper triangle'''
 
     augmented_matrix: np.ndarray = np.concatenate((matrix_a, matrix_b), axis=1).astype(float)
-
+    msg_soln = []
     #? Debugging: show augemented matrix
-    print("Augemented Matrix (Initial): ")
-    print(augmented_matrix)
-    print()
+    msg_soln.append("Augemented Matrix (Initial): ")
+    msg_soln.append(f"{augmented_matrix}")
+    msg_soln.append(" ")
+
 
 
     #* partial pivot
@@ -49,18 +48,19 @@ def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> np.ndarray:
         if max_row != i:
 
             augmented_matrix[[i, max_row]] = augmented_matrix[[max_row, i]]
-            print(f"Swapped rows {i} and {max_row}:")
-            print(augmented_matrix)
-            print()
+            msg_soln.append(f"Swapped rows {i} and {max_row}:")
+            msg_soln.append(f"{augmented_matrix}")
+            msg_soln.append(" ")
 
-    print("Solution:")
+    msg_soln.append("Solution: ")
 
 
     for i in range(matrix_b.size):
 
         #! 0 on pivot points (unsolvable)
         if augmented_matrix[i][i] == 0.0:
-            raise ZeroDivisionError
+            print("error: ZeroDivisionError")
+            return None
 
 
         #? formula R_j = R_j - (scaling factor * R_i)
@@ -71,17 +71,17 @@ def forward_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> np.ndarray:
 
             #? Debugging: show augmented matrix
             # print(f"{i}, {j}, {augmented_matrix[j][i]}, {augmented_matrix[i][i]}, {scaling_factor}")
-            print(f"Scaling factor: {scaling_factor}")
-            print(augmented_matrix)
-            print()
+            # print(f"Scaling factor: {scaling_factor}")
+            msg_soln.append(f"{augmented_matrix}")
+            msg_soln.append(" ")
 
         #? Debugging: show augmented matrix
-        print()
-        print(augmented_matrix)
+        msg_soln.append(f"{augmented_matrix}")
+        msg_soln.append(" ")
 
-    return augmented_matrix
+    return (augmented_matrix, msg_soln)
 
-def gaussian_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> None:
+def gaussian_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> list[str]:
     '''
     Main Function for Gaussian elimination program
     '''
@@ -93,15 +93,24 @@ def gaussian_elimination(matrix_a:np.ndarray, matrix_b:np.ndarray) -> None:
     #! constant matrix is not compatible with coef matrix
     if not is_compatible_matrix(matrix_a,matrix_b):
         return
+    msg_soln = []
+    aug_matrix, soln1 = forward_elimination(matrix_a,matrix_b)
+    # for i in msg_soln:
+    #     print(i)
+    msg_soln.extend(soln1)
+    answers, soln2 = backwards_substitution(aug_matrix,aug_matrix.shape[1] - 1)
+    msg_soln.append(soln2)
 
-    aug_matrix = forward_elimination(matrix_a,matrix_b)
-    answers = backwards_substitution(aug_matrix,aug_matrix.shape[1] - 1)
     ans_frac = [Fraction(x).limit_denominator() for x in answers]
 
-    print(answers)
-    print(ans_frac)
+    msg_soln.append(str(ans_frac))
 
-    return None
+    for i in msg_soln:
+        print(i)
+    # print(answers)
+    # print(ans_frac)
+
+    return msg_soln
 
 
 def main() -> None:
@@ -124,12 +133,14 @@ def main() -> None:
     #               [Fraction(-1,8),Fraction(7,48)]])
     # b = np.array([[Fraction(6,1)],
     #               [Fraction(-1,1)]])
-    a = np.array([[0,-5,-10],
-                  [45,0,-4],
-                  [-10,-4,0]])
+    a = np.array([[0,5,10],
+                  [45,0,4],
+                  [10,4,0]])
     b = np.array([[130],
                   [106],
                   [0]])
+    # a = np.array([[1]])
+    # b = np.array([[2]])
     gaussian_elimination(a,b)
     return None
 
